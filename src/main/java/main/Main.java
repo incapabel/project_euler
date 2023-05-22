0f0ac823186456bc069c58c47a894bb69c6a600d
incapabel <bobdeen7@gmail.com>
2023-05-22 16:23:48 +0200

    Code that should trigger workflow

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,8 @@ import problems.Problem14;
 import utils.Problem;
 
 public class Main {
+	private final int ZERO_ = 0;
+
 	public static void main(String[] args) {
 		Problem problem = new Problem14();
 		System.out.println(problem.solve());
