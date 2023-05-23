14dfce6ced95bd6dfb0d31d19dc7e4f1d30da1ed
incapabel <bobdeen7@gmail.com>
2023-05-23 18:41:43 +0200

    test branch

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,7 @@
 package main;
 
-import problems.Problem14;
-import utils.Problem;
-
 public class Main {
-	private final int ZERO_ = 0;
-
 	public static void main(String[] args) {
-		Problem problem = new Problem14();
-		System.out.println(problem.solve());
+		System.out.println("test");
 	}
 }
--- src/main/java/utils/Problem.java
+++
@@ -1,5 +0,0 @@
-package utils;
-
-public interface Problem {
-	String solve();
-}
